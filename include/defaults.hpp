2c2eccf571e25e5794bb793fb1b538b060dcc50e
jmbills <jasonm.bills@yahoo.com>
2023-09-12 14:42:26 -0600

    Rename CATERR to IERR (#13)
    CATERR is ambiguous because it represents two types of errors: IERR and MCERR. This
    changes the name to IERR to be more specific.
    
    Signed-off-by: Jason M. Bills <jason.m.bills@intel.com>

--- include/defaults.hpp
+++ include/defaults.hpp
@@ -228,7 +228,7 @@ inline void loadSettings(sdbusplus::asio::object_server &objectServer,
         objectServer, "/xyz/openbmc_project/control/processor_error_config",
         "xyz.openbmc_project.Control.Processor.ErrConfig");
 
-    setting->addProperty("ResetOnCATERR", false);
+    setting->addProperty("ResetOnIERR", false);
     setting->addProperty("ResetOnERR2", false);
     setting->addProperty("ErrorCountCPU1", static_cast<uint8_t>(0));
     setting->addProperty("ErrorCountCPU2", static_cast<uint8_t>(0));
